17f39a777cca2352a6e9d14110510c9412532224
piyushrpt <piyushrpt@users.noreply.github.com>
2019-02-15 14:19:45 -0800

    using conda variables

--- travis/pyre/build.sh
+++ travis/pyre/build.sh
@@ -4,6 +4,13 @@
 
 mkdir $SRC_DIR/{build,install}
 
+##Workaround for PROJ_LIB clobbering by mm internally
+export RECIPE_CONDA_PROJ_LIB=$PROJ_LIB
+unset PROJ_LIB
+
+##PYTHON is also clobbered by mm in configuration
+export RECIPE_CONDA_PYTHON=$PYTHON
+
 export BLD_CONFIG=$SRC_DIR/config
 export EXPORT_ROOT=$PREFIX
 export PATH=$PATH:$PREFIX/bin
@@ -14,18 +21,20 @@ export MM_LIBPATH=$PREFIX/lib
 
 cd $SRC_DIR/pyre
 
-export PYTHON=`python3 -c "import sysconfig; print(sysconfig.get_config_var('CONFINCLUDEPY').split('/')[-1])"`
-export PYTHON_INCDIR=`python3 -c "from sysconfig import get_paths; print(get_paths()['include'])"`
+export PYTHON=`$RECIPE_CONDA_PYTHON -c "import sysconfig; print(sysconfig.get_config_var('CONFINCLUDEPY').split('/')[-1])"`
+export PYTHON_INCDIR=`$RECIPE_CONDA_PYTHON -c "from sysconfig import get_paths; print(get_paths()['include'])"`
 export PYTHON_LIB=$PYTHON
-export PYTHON_LIBDIR=`python3 -c "from sysconfig import get_config_var; print(get_config_var('LIBDEST'))"`
+export PYTHON_LIBDIR=`$RECIPE_CONDA_PYTHON -c "from sysconfig import get_config_var; print(get_config_var('LIBDEST'))"`
 export PYTHON_PYCFLAGS=-b
 
-python3 $SRC_DIR/config/make/mm.py build
+$RECIPE_CONDA_PYTHON $SRC_DIR/config/make/mm.py build
 
 # symlink libraries, binaries, python packages, etc.
-pythonsite=`python3 -c "import site; print(site.getsitepackages()[0])"`
-echo $pythonsite
-echo `ls $PREFIX`
-mv $PREFIX/packages/* $pythonsite
+mv $PREFIX/packages/* $SP_DIR
 rm -d $PREFIX/packages
 
+##Restore environment
+export PROJ_LIB=$RECIPE_CONDA_PROJ_LIB
+export PYTHON=$RECIPE_CONDA_PYTHON
+unset RECIPE_CONDA_PROJ_LIB
+unset RECIPE_CONDA_PYTHON
