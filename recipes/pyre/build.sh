ab840586ae4e9e1f414512893d6fa5d739526f8f
piyushrpt <piyushrpt@users.noreply.github.com>
2019-02-21 11:36:11 -0800

    Using conda compilers for pyre

--- recipes/pyre/build.sh
+++ recipes/pyre/build.sh
@@ -10,28 +10,40 @@ unset PROJ_LIB
 
 ##PYTHON is also clobbered by mm in configuration
 export RECIPE_CONDA_PYTHON=$PYTHON
-
 export BLD_CONFIG=$SRC_DIR/config
-export EXPORT_ROOT=$PREFIX
-export PATH=$PATH:$PREFIX/bin
-export PYTHONPATH=$PYTHONPATH:$PREFIX/packages
-export LD_LIBRARY_PATH=$LD_LIBRARYPATH:$PREFIX/lib
-export MM_INCLUDES=$PREFIX/include
-export MM_LIBPATH=$PREFIX/lib
+export EXPORT_ROOT=$SRC_DIR/install
+export PATH=$PATH:$EXPORT_ROOT/bin
+export PYTHONPATH=$PYTHONPATH:$EXPORT_ROOT/packages
+export LD_LIBRARY_PATH=$LD_LIBRARYPATH:$EXPORT_ROOT/lib
+export MM_INCLUDES=$EXPORT_ROOT/include
+export MM_LIBPATH=$EXPORT_ROOT/lib
 
+
+touch $PREFIX/include/portinfo
 cd $SRC_DIR/pyre
 
-export PYTHON=`$RECIPE_CONDA_PYTHON -c "import sysconfig; print(sysconfig.get_config_var('CONFINCLUDEPY').split('/')[-1])"`
-export PYTHON_INCDIR=`$RECIPE_CONDA_PYTHON -c "from sysconfig import get_paths; print(get_paths()['include'])"`
-export PYTHON_LIB=$PYTHON
-export PYTHON_LIBDIR=`$RECIPE_CONDA_PYTHON -c "from sysconfig import get_config_var; print(get_config_var('LIBDEST'))"`
-export PYTHON_PYCFLAGS=-b
 
+cat <<EOF >> .mm/config.def
+PYTHON=`$RECIPE_CONDA_PYTHON -c "import sysconfig; print(sysconfig.get_config_var('CONFINCLUDEPY').split('/')[-1])"`
+PYTHON_DIR=$PREFIX
+PYTHON_INCDIR=`$RECIPE_CONDA_PYTHON -c "from sysconfig import get_paths; print(get_paths()['include'])"`
+PYTHON_LIB=$PYTHON
+PYTHON_LIBDIR=`$RECIPE_CONDA_PYTHON -c "from sysconfig import get_config_var; print(get_config_var('LIBDEST'))"`
+PYTHON_PYCFLAGS=-b
+EOF
+
+export COMPILER_CXX_NAME=$CXX
+
+echo "MM ENVIRONMENT-----------"
+env
+echo "MM END ------------------"
 $RECIPE_CONDA_PYTHON $SRC_DIR/config/make/mm.py build
 
 # symlink libraries, binaries, python packages, etc.
-mv $PREFIX/packages/* $SP_DIR
-rm -d $PREFIX/packages
+mv $EXPORT_ROOT/packages/* $SP_DIR
+mv $EXPORT_ROOT/include/* $PREFIX/include/
+mv $EXPORT_ROOT/bin/* $PREFIX/bin/
+mv $EXPORT_ROOT/lib/* $PREFIX/lib/ 
 
 ##Restore environment
 export PROJ_LIB=$RECIPE_CONDA_PROJ_LIB
